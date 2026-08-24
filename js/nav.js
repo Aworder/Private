eac0d09c0619091025a03f911acd64c1da8ed87f
Aworder <2389744253@qq.com>
2026-08-24 17:03:02 +0800

    模块化修改

--- js/nav.js
+++ js/nav.js
@@ -1,4 +1,5 @@
-(function () {
+javascript
+export function initNav() {
   var path = location.pathname.split("/").pop() || "index.html";
   var links = document.querySelectorAll(".nav-link");
   for (var i = 0; i < links.length; i++) {
@@ -6,4 +7,4 @@
     if (href === path) links[i].classList.add("active");
     else links[i].classList.remove("active");
   }
-})();
+}
